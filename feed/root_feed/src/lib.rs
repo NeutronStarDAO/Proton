bb5800f0c9ccb815f6ec79c1acdb23dd4814c6a8
xiaoyuanxun <2060363684@qq.com>
2024-08-20 22:44:19 +0800

    wallet

--- feed/root_feed/src/lib.rs
+++ feed/root_feed/src/lib.rs
@@ -1,4 +1,7 @@
-use candid::{Principal, Encode};
+mod wallet;
+use wallet::{TransferResult, WalletTX};
+
+use candid::{Principal, Nat, Encode};
 use ic_cdk::api::management_canister::main::{CanisterStatusResponse, CanisterIdRecord};
 use ic_cdk::api::management_canister::main::{
     create_canister, CreateCanisterArgument, CanisterSettings,
