7fb3ab075639939deb0ea3c282f5bde374ce12f7
Nash Zheng <nashzhengpro@gmail.com>
2024-09-23 19:28:25 +0800

    Testnet Statement

--- src/components/Setting/index.tsx
+++ src/components/Setting/index.tsx
@@ -30,9 +30,9 @@ export const Settings = () => {
       <div className={"setting_button"}>
 
         <span style={{background: theme === "light" ? "#A3C4FF" : ""}} onClick={() => handleClick("light")}>Light</span>
-        <span style={{background: theme === "dark" ? "#A3C4FF" : "", color: theme === "dark" ? "wheat" : ""}}
+        <span style={{background: theme === "dark" ? "#A3C4FF" : "", color: theme === "dark" ? "#000" : ""}}
               onClick={() => handleClick("dark")}>Dark</span>
-        <span style={{background: theme === "auto" ? "#A3C4FF" : "", color: theme === "auto" ? "wheat" : ""}}
+        <span style={{background: theme === "auto" ? "#A3C4FF" : "", color: theme === "auto" ? "" : ""}}
               onClick={() => handleClick("auto")}>Auto</span>
       </div>
     </div>
@@ -43,8 +43,25 @@ export const Settings = () => {
       </div>
       <div style={{cursor: "pointer"}} className={"setting_button"}>
         <span onClick={() => logOut?.()}>Log Out</span>
-        <span>Delete Account</span>
       </div>
     </div>
+
+    {/* Testnet Statement */}
+    <div className={"setting_item"}>
+      <details>
+        <summary>
+          Testnet Statement
+          <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#fff" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></circle><circle cx="128" cy="180" r="12"></circle><path d="M127.9995,144.0045v-8a28,28,0,1,0-28-28" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
+        </summary>
+        <div>
+        Our goal is to create a decentralized app (DApp), but the journey toward full decentralization is a gradual process. It cannot be achieved overnight. As is widely known, DApps on the Internet Computer (IC) require a DAO to govern them and the integration of AI for content moderation. Until these technologies are fully implemented, the IceCube team will be responsible for overseeing content management.
+        <br></br><br></br>
+        It is no secret that the internet is filled with misinformation, fake promotions, scams, and malicious actors. We are committed to ensuring that our community does not become a breeding ground for the promotion of violence, extremism, terrorism, pornography, or subversive content.
+        <br></br><br></br>
+        Therefore, until DAO governance and decentralized AI content moderation are in place, the IceCube team will take responsibility for removing inappropriate content from public view. Please note, however, that this action only hides the content from the public, while the original poster will still have access to their own content. No direct deletion will take place.
+        </div>
+      </details>
+    </div>
+    {/* Testnet Statement */}
   </div>
 }
